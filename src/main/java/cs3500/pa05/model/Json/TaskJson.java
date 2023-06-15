bfc3d947335a7fb8570d067175643ae463411faf
dandan51332 <danielle51332@gmail.com>
2023-06-15 15:21:33 -0400

    Created the taskJson Fixes #39

--- src/main/java/cs3500/pa05/model/Json/TaskJson.java
+++ src/main/java/cs3500/pa05/model/Json/TaskJson.java
@@ -1,4 +1,20 @@
 package cs3500.pa05.model.Json;
 
-public record TaskJson() {
+import com.fasterxml.jackson.annotation.JsonProperty;
+import cs3500.pa05.model.Day;
+import cs3500.pa05.model.Status;
+
+/**
+ * Represents a task that the user needs to remember.
+ *
+ * @param name the name of the task
+ * @param description a description of the task
+ * @param day the day the task needs to be done by
+ * @param status the current status of the task
+ */
+public record TaskJson(@JsonProperty("name") String name,
+                       @JsonProperty("description") String description,
+                       @JsonProperty("day") Day day,
+                       @JsonProperty("completion")Status status) {
+  //TODO: New Tasks should be marked as not complete by default. Descriptions are optional.
 }
