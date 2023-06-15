7e6ef006e8b41d4baa83331f6d397768fbd4fbb5
dandan51332 <danielle51332@gmail.com>
2023-06-15 15:58:09 -0400

    implemented toString for time class Fixes #41

--- src/main/java/cs3500/pa05/model/Time.java
+++ src/main/java/cs3500/pa05/model/Time.java
@@ -1,4 +1,50 @@
 package cs3500.pa05.model;
 
+/**
+ * Represents a timestamp
+ */
 public class Time {
+  private static final int MAX_MILITARY_HOURS = 24;
+  private static final int MAX_MINUTES = 59;
+  private int hour;
+  private int minute;
+  private RegularTimeMarkers marker = null;
+
+  /**
+   * Instantiates a new timestamp in military time.
+   *
+   * @param hour the hour of the timestamp
+   * @param minute the minute of the timestamp
+   */
+  public Time(int hour, int minute){
+    if(hour > MAX_MILITARY_HOURS || hour < 0){
+      throw new IllegalArgumentException("Hour given is out of bounds.");
+    }
+    if(minute > MAX_MINUTES || minute < 0){
+      throw new IllegalArgumentException("Minutes given is out of bounds.");
+    }
+  }
+
+  /**
+   * Instantiates a new timestamp in regular time.
+   *
+   * @param hour the hour of the timestamp
+   * @param minute the minute of the timestamp
+   * @param marker the marker for the timestamp
+   */
+  public Time(int hour, int minute, RegularTimeMarkers marker){
+    this(hour, minute);
+    if(hour > 12 || hour < 0){
+      throw new IllegalArgumentException("Hour given is out of bounds.");
+    }
+    this.marker = marker;
+  }
+
+  @Override
+  public String toString(){
+    if(marker != null){
+      return  String.format("%d:%d %s", hour, minute, marker.toString().toLowerCase());
+    }
+    return String.format("%d:%d", hour, minute);
+  }
 }
