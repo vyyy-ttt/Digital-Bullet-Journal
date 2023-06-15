93dc34d3b09e3b965da9426cf06dc4e75467094e
hungrybellie <annabelle.chung2236@gmail.com>
2023-06-14 21:38:51 -0700

    new new new UML + json

--- src/main/java/cs3500/pa05/model/WeekJson.java
+++ src/main/java/cs3500/pa05/model/WeekJson.java
@@ -1,4 +1,20 @@
 package cs3500.pa05.model;
 
-public record WeekJson() {
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+/**
+ * JSON format of this record:
+ * <p>
+ * <code>
+ * {
+ * "week": "week",
+ * }
+ * </code>
+ * </p>
+ *
+ * @param week DayJson[] of the week
+ */
+public record WeekJson(
+    @JsonProperty("week") DayJson[] week
+) {
 }
