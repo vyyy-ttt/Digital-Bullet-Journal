e3113b1ab7c7d3dd357f60cc429a37b5a81653ce
dandan51332 <danielle51332@gmail.com>
2023-06-15 20:11:50 -0400

    deleted extra files
    Fixes #46

--- src/main/java/cs3500/pa05/model/maybedelete/Event.java
+++
@@ -1,7 +0,0 @@
-package cs3500.pa05.model.maybedelete;
-
-/**
- * Represents an event.
- */
-public class Event {
-}
--- src/main/java/cs3500/pa05/model/maybedelete/Week.java
+++
@@ -1,7 +0,0 @@
-package cs3500.pa05.model.maybedelete;
-
-/**
- * Represents a week.
- */
-public class Week {
-}
