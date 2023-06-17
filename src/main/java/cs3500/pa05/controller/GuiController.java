9f611c8440b2370c27e42ba3e0079fc0a47fa856
hungrybellie <annabelle.chung2236@gmail.com>
2023-06-17 00:26:54 -0700

    text field only accepts bujo files, if not the label prompts them to try again

--- src/main/java/cs3500/pa05/controller/GuiController.java
+++ src/main/java/cs3500/pa05/controller/GuiController.java
@@ -3,6 +3,7 @@ package cs3500.pa05.controller;
 import cs3500.pa05.view.BujoView;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
@@ -15,11 +16,15 @@ public class GuiController {
   @FXML
   private TextField bujoFileField;
   @FXML
+  private Label prompt;
+  @FXML
   private Stage stage;
   private BujoView bujoView = new BujoView();
 
   public GuiController(Stage stage) {
     goButton = new Button();
+    bujoFileField = new TextField();
+    prompt = new Label();
     this.stage = stage;
   }
 
@@ -31,17 +36,21 @@ public class GuiController {
   }
 
   /**
-   * Hanldes the user input in the textField for the bujo file path.
+   * Handles the user input in the textField for the bujo file path.
    */
   private void handleBujoField() {
-    //TODO verify the bujo field here
+    String input = bujoFileField.getText();
+    if (input.endsWith(".bujo")) {
+      handleGoButton();
+    } else {
+      prompt.setText("Oops! That wasn't a valid bujo file, please try again.");
+    }
   }
 
   /**
    * Initializes the event handlers for controls of the Gui.
    */
   public void run() {
-    goButton.setOnAction(event -> handleGoButton());
-
+    goButton.setOnAction(event -> handleBujoField());
   }
 }
