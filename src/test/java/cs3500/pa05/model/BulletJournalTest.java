a3e60969ebed8d338c943a5d639e1edcf67ff727
vyyy-ttt <truong.vy@northeastern.edu>
2023-06-19 23:04:44 -0400

    Added BulletJournalTest test method signatures.

--- src/test/java/cs3500/pa05/model/BulletJournalTest.java
+++ src/test/java/cs3500/pa05/model/BulletJournalTest.java
@@ -14,4 +14,74 @@ class BulletJournalTest {
         new FileWriter(Path.of("bujoTest.bujo").toFile()));
   }
 
+  @Test
+  public void addEventTest() {
+
+  }
+
+  @Test
+  public void addTaskTest() {
+
+  }
+
+  @Test
+  public void removeEventTest() {
+
+  }
+
+  @Test
+  public void
+  removeTaskTest() {
+
+  }
+
+  @Test
+  public void sortTasksPriorityTest() {
+
+  }
+
+  @Test
+  public void convertToMinutesTest() {
+
+  }
+
+  @Test
+  public void sortTasksNameDurationTest() {
+
+  }
+
+  @Test
+  public void sortEventsNameDurationTest() {
+
+  }
+
+  @Test
+  public void checkLimitViolationTest() {
+
+  }
+
+  @Test
+  public void setTaskLimitTest() {
+
+  }
+
+  @Test
+  public void setEventLimitTest() {
+
+  }
+
+  @Test
+  public void chooseThemeTest() {
+
+  }
+
+  @Test
+  public void saveBulletJournalTest() {
+
+  }
+
+  @Test
+  public void getWeekTest() {
+
+  }
 }
