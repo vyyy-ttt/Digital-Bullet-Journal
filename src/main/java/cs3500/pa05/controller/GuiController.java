913c0e79ba28a45d6aa7866a40fbdeeb6cd20422
hungrybellie <annabelle.chung2236@gmail.com>
2023-06-19 22:40:35 -0700

    event and task popups set up for logic stuff

--- src/main/java/cs3500/pa05/controller/GuiController.java
+++ src/main/java/cs3500/pa05/controller/GuiController.java
@@ -8,6 +8,12 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
 import javafx.stage.Popup;
 import javafx.stage.Stage;
 
@@ -50,6 +56,15 @@ public class GuiController {
   private Button cancelEvent;
   private Stage stage;
   private Popup popup;
+  private TextField name;
+  private TextField description;
+  private TextField hoursDigit;
+  private TextField minutesDigit;
+  private Label hoursLabel;
+  private Label minutesLabel;
+  private Button addTask;
+  private TextField day;
+  private TextField duration;
   private BulletJournal bulletJournal;
 
   public GuiController(Stage stage) {
@@ -58,46 +73,102 @@ public class GuiController {
   }
 
   /**
-   * Loads a popup for the user to input the details of a task.
+   * Makes the GUI elements of a task popup.
    */
-  public void handleTaskPopup() {
-    try {
-      FXMLLoader loader = new FXMLLoader();
-      loader.setLocation(getClass().getClassLoader().getResource("taskPop.fxml"));
-      Scene scene = loader.load();
-      popup.getContent().add(scene.getRoot());
-      cancel = new Button("cancel");
-      cancel.setOnAction(event -> popup.hide());
-      popup.getContent().add(cancel);
-    } catch (IOException e) {
-      System.out.println(e.getMessage());
-    }
+  private void makeTaskPopUp() {
+    Rectangle background = new Rectangle();
+    background.setHeight(160);
+    background.setWidth(180);
+    background.setFill(Color.valueOf("#E7EACD"));
+    background.setArcHeight(20);
+    background.setArcWidth(20);
+    VBox vBox = new VBox(8);
+    Rectangle padding = new Rectangle(180, 10);
+    padding.setFill(Color.valueOf("#E7EACD"));
+    vBox.getChildren().add(padding);
+    name = new TextField("task name...");
+    description = new TextField("description...");
+    vBox.getChildren().add(name);
+    vBox.getChildren().add(description);
+    HBox hBox = new HBox(5);
+    hoursDigit = new TextField("___");
+    hoursDigit.setPrefWidth(30);
+    hoursLabel = new Label("H");
+    minutesDigit = new TextField("___");
+    minutesDigit.setPrefWidth(30);
+    minutesLabel = new Label("M");
+    hBox.getChildren().add(hoursDigit);
+    hBox.getChildren().add(hoursLabel);
+    hBox.getChildren().add(minutesDigit);
+    hBox.getChildren().add(minutesLabel);
+    vBox.getChildren().add(hBox);
+    HBox buttonRow = new HBox(5);
+    addTask = new Button("add task");
+    addTask.setOnAction(event -> addToGridPane(name.getText()));
+    cancel = new Button("cancel");
+    cancel.setOnAction(event -> popup.hide());
+    buttonRow.getChildren().add(addTask);
+    buttonRow.getChildren().add(cancel);
+    vBox.getChildren().add(buttonRow);
+    popup.getContent().add(background);
+    popup.getContent().add(vBox);
   }
 
   /**
-   * Loads a popup for the user to input the details of an event.
+   * Makes the GUI elements of an event popup.
    */
-  public void handleEventPopup() {
-    try {
-      FXMLLoader loader = new FXMLLoader();
-      loader.setLocation(getClass().getClassLoader().getResource("eventPop.fxml"));
-      Scene scene = loader.load();
-      popup.getContent().add(scene.getRoot());
-      cancelEvent = new Button("cancel");
-      cancelEvent.setOnAction(event -> popup.hide());
-      popup.getContent().add(cancelEvent);
-    } catch (IOException e) {
-      System.out.println(e.getMessage());
-    }
+  private void makeEventPopUp() {
+    Rectangle background = new Rectangle();
+    background.setHeight(220);
+    background.setWidth(180);
+    background.setFill(Color.valueOf("#E7EACD"));
+    background.setArcHeight(20);
+    background.setArcWidth(20);
+    VBox vBox = new VBox(8);
+    Rectangle padding = new Rectangle(180, 10);
+    padding.setFill(Color.valueOf("#E7EACD"));
+    vBox.getChildren().add(padding);
+    name = new TextField("event name...");
+    description = new TextField("description...");
+    day = new TextField("day...");
+    duration = new TextField("duration...");
+    vBox.getChildren().add(name);
+    vBox.getChildren().add(description);
+    vBox.getChildren().add(day);
+    vBox.getChildren().add(duration);
+    HBox hBox = new HBox(5);
+    hoursDigit = new TextField("___");
+    hoursDigit.setPrefWidth(30);
+    hoursLabel = new Label("H");
+    minutesDigit = new TextField("___");
+    minutesDigit.setPrefWidth(30);
+    minutesLabel = new Label("M");
+    hBox.getChildren().add(hoursDigit);
+    hBox.getChildren().add(hoursLabel);
+    hBox.getChildren().add(minutesDigit);
+    hBox.getChildren().add(minutesLabel);
+    vBox.getChildren().add(hBox);
+    HBox buttonRow = new HBox(5);
+    addTask = new Button("add event");
+    addTask.setOnAction(event -> addToGridPane(name.getText()));
+    cancel = new Button("cancel");
+    cancel.setOnAction(event -> popup.hide());
+    buttonRow.getChildren().add(addTask);
+    buttonRow.getChildren().add(cancel);
+    vBox.getChildren().add(buttonRow);
+    popup.getContent().add(background);
+    popup.getContent().add(vBox);
   }
 
   /**
    * Adds a task or event to the week gridpane.
    */
-  public void addToGridPane() {
+  public void addToGridPane(String name) {
+    monPane.setText(name);
     //TODO implement so that depending on the day the person chooses,
     // it will add the task to the necessary pane.
   }
+
   /**
    * Saves the current bullet jouranl spread.
    */
@@ -139,9 +210,9 @@ public class GuiController {
 
   public void run() {
     addTaskPG.setOnAction(event -> makePopup());
-    handleTaskPopup();
+    makeTaskPopUp();
     addEventPG.setOnAction(event -> makePopup());
-    handleEventPopup();
+    makeEventPopUp();
     savePG.setOnAction(event -> handleSaveButton());
     setLimitPG.setOnAction(event -> handleSetLimit());
     changeThemePG.setOnAction(event -> handleChangeTheme());
