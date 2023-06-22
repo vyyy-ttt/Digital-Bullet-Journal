f6f260941242c8e22186cf2da83f6d28768f53d8
vyyy-ttt <truong.vy@northeastern.edu>
2023-06-21 22:50:31 -0400

    Updated javadoc for BujoJson.

--- src/main/java/cs3500/pa05/model/Json/BujoJson.java
+++ src/main/java/cs3500/pa05/model/Json/BujoJson.java
@@ -4,9 +4,13 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import cs3500.pa05.model.ThemeType;
 
 /**
+ * Represents a Bujo as a Json.
+ *
  * @param week   an array of the days of the week
+ * @param weekName name of the week
  * @param limits the limits for events and tasks per day
  * @param theme  the theme for the bullet journal
+ * @param note the note on the bullet journal
  */
 public record BujoJson(
     @JsonProperty("week") DayJson[] week,
